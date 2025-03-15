0487f8a0e359bcbba8cb9228c8b2171ebf96ec3b
francoz16 <www.chezfrancoz11@hotmail.com>
2025-03-15 19:56:11 -0300

    Agregardo de Primer entidad

---
+++ diseno/diseno/src/main/java/com/example/diseno/repository/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.example.diseno.repository;
+
+import com.example.diseno.entity.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
+}
